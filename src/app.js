d18984533008f7fbaa01c143a4cf30c6a073a4a1
Sergioalv15 <sergioalv961@gmail.com>
2025-08-12 10:23:06 +0000

    [Corregido lineamiento codigo y añadida nueva funcion] Se ha modificado el codigo, eliminando lineas sin utilidad, reposicionando los contenedores let dentro de la funcion para una mejor agrupacion sin variables globales y añadida funcion de cambio de color con cada request al boton.

--- src/app.js
+++ src/app.js
@@ -5,39 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// window.onload = function() {
-//   //write your code here
-//   console.log("Hello Rigo from the console!");
-
-// }
-
-let excusa = document.getElementById("excusa")
-let boton = document.getElementById("boton")
-
-let quien = ["El perro", "Michael Jackson","Patricia","El profe","Un amigo","Thanos"]
-let cuando = ["ayer","justo antes de vernos","esta mañana","anoche","en este preciso instante","cuando menos me lo esperaba"]
-let que = ["se ha caido por las escaleras","me ha suspendido el proyecto","los meniscos me duelen","el futuro es incierto","no tenia internet","me ha fulminado"]
-
-
-const generadorExcusa = () => {
-  const quienAleatorio = quien[Math.floor(Math.random()*6)]
-  const cuandoAleatorio = cuando[Math.floor(Math.random()*6)]
-  const queAleatorio = que[Math.floor(Math.random()*6)]
-
-  return `${quienAleatorio} ${cuandoAleatorio} ${queAleatorio}`
-} 
-
-excusa.textContent = generadorExcusa()
 
 boton.addEventListener("click", () => {
+  let boton = document.getElementById("boton")
+  let excusa = document.getElementById("excusa")
+  
+  let quien = ["El perro", "Michael Jackson","Patricia","El profe","Un amigo","Thanos"]
+  let cuando = ["ayer","justo antes de vernos","esta mañana","anoche","en este preciso instante","cuando menos me lo esperaba"]
+  let que = ["se ha caido por las escaleras","me ha suspendido el proyecto","los meniscos me duelen","el futuro es incierto","no tenia internet","me ha fulminado"]
+
   const generadorExcusa = () => {
-    const quienAleatorio = quien[Math.floor(Math.random()*6)]
-    const cuandoAleatorio = cuando[Math.floor(Math.random()*6)]
-    const queAleatorio = que[Math.floor(Math.random()*6)]
+    const quienAleatorio = quien[Math.floor(Math.random()* quien.length)]
+    const cuandoAleatorio = cuando[Math.floor(Math.random()* cuando.length)]
+    const queAleatorio = que[Math.floor(Math.random()* que.length)]
   
     return `${quienAleatorio} ${cuandoAleatorio} ${queAleatorio}`
-  } 
-
+  }
   excusa.textContent = generadorExcusa()
+    
+  const cambiarColorDeFondo = () => {
+    const colores = ["blue","red","green","grey","beige"]
+    return excusa.style.backgroundColor = colores[Math.floor(Math.random()* colores.length)]
+  }
+  cambiarColorDeFondo()
 })
-
